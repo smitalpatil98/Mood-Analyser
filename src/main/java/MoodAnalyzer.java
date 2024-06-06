6ad66c5b9336a9e91c12e8049af8541cdf91b032
smitalpatil98 <patilsmital.1101998gmail.com>
2024-06-06 16:12:02 +0530

    moodanalyzer

---
+++ src/main/java/MoodAnalyzer.java
@@ -0,0 +1,16 @@
+
+public class MoodAnalyzer
+{
+
+
+    public String analyzemood(String msg)
+    {
+        if(msg.contains("sad")){
+            return "SAD";
+        }
+        else
+        {
+            return "HAPPY";
+        }
+    }
+    }
---
+++ src/test/java/MoodAnalyzerTest.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+class MoodAnalyzerTest
+{
+@Test
+    void analyzeMood()
+{
+    MoodAnalyzer moodAnalyzer1 = new MoodAnalyzer();
+    String mood = moodAnalyzer1.analyzemood("I am in Sad mood".toLowerCase());
+    Assertions.assertEquals(mood,"SAD");
+}
+}
