d4c5242cd73abb1fca2b8e9c38095ce32c39d58f
smitalpatil98 <patilsmital.1101998gmail.com>
2024-06-06 17:44:17 +0530

    TC1.1-1.2_message_in_constructor

--- src/main/java/MoodAnalyzer.java
+++ src/main/java/MoodAnalyzer.java
@@ -1,12 +1,23 @@
 
 public class MoodAnalyzer
 {
+    private String msg;
+    //Zero argument constructor
+    public MoodAnalyzer()
+    {
+    }
+    //Parameterized Constructor
+    public MoodAnalyzer(String msg)
+    {
+        this.msg = msg;
+    }
     public String analyzemood(String msg)
     {
         if(msg.contains("sad")){
             return "SAD";
         }
-        else {
+        else
+        {
             return "HAPPY";
         }
     }
--- src/test/java/MoodAnalyzerTest.java
+++ src/test/java/MoodAnalyzerTest.java
@@ -7,7 +7,13 @@ class MoodAnalyzerTest
     void analyzeMood()
     {
         MoodAnalyzer moodAnalyzer1 = new MoodAnalyzer();
+        String mood = moodAnalyzer1.analyzemood("I am in sad mood".toLowerCase());
+        Assertions.assertEquals(mood,"SAD");
+    }
+    void analyzeHappyMood()
+    {
+        MoodAnalyzer moodAnalyzer1 = new MoodAnalyzer();
         String mood = moodAnalyzer1.analyzemood("I am in happy mood".toLowerCase());
-        Assertions.assertEquals(mood,"HAPPY");
+        Assertions.assertEquals(mood,"Happy");
     }
 }
